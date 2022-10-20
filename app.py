8e11de84d7b7b623d3099d94f6c0193f093501d3
pandeymilind <77435269+pandeymilind@users.noreply.github.com>
2022-10-20 21:52:08 +0530

    file template

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
+
 from flask import Flask,redirect,url_for,render_template,request
 app=Flask(__name__)
 
 
 
-@app.route("/")
-def home():
-    return "Hello"
+@app.route("/signin",methods=["POST","GET"])
+def signin():
+    return render_template("signin.html")
+
 
 
 if __name__=="__main__":
